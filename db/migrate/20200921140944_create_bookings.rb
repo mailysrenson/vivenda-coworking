ad5729452c5fc22d2c7db44621b4e07c1e89ae8f
Nicolas Emmery <nicolas.emmery@gmail.com>
2020-09-21 16:26:10 +0200

    db migration

--- db/migrate/20200921140944_create_bookings.rb
+++ db/migrate/20200921140944_create_bookings.rb
@@ -1,18 +1,23 @@
 class CreateBookings < ActiveRecord::Migration[6.0]
   def change
     create_table :bookings do |t|
-      t.casas :references
+      t.references :casas
       t.string :last_name
       t.string :first_name
       t.string :email
       t.string :phone
       t.integer :adults
       t.integer :childrens
-      t.boolean :baby_bed
+      t.boolean :baby_bed, default: false
       t.date :arrival_date
       t.date :departure_date
       t.string :comment
-      t.string :status
+      t.string :status, default: "Pending"
+      t.string :street
+      t.string :city
+      t.integer :zipcode
+      t.string :country
+
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_09_21_140944) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "casas_id"
+    t.string "last_name"
+    t.string "first_name"
+    t.string "email"
+    t.string "phone"
+    t.integer "adults"
+    t.integer "childrens"
+    t.boolean "baby_bed", default: false
+    t.date "arrival_date"
+    t.date "departure_date"
+    t.string "comment"
+    t.string "status", default: "Pending"
+    t.string "street"
+    t.string "city"
+    t.integer "zipcode"
+    t.string "country"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["casas_id"], name: "index_bookings_on_casas_id"
+  end
+
+  create_table "casas", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
