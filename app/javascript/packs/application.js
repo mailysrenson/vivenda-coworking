ec624215790c5c6a46004208c8929af63ae49026
Alexis Duchatelet <alexis.duchatelet@gmail.com>
2020-09-22 15:54:38 +0200

    flatpickr functional

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,7 +6,8 @@
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
-require("channels")
+require("channels");
+require("flatpickr");
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -27,6 +28,7 @@ import "bootstrap";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
+import { initFlatPicker } from "../plugins/flatpickr";
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
