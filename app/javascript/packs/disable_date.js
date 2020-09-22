bf634c69174454f01c421758cd89ab5d3d2c8048
Nicolas Emmery <nicolas.emmery@gmail.com>
2020-09-22 16:56:10 +0200

    restriction pseudo code

---
+++ app/javascript/packs/disable_date.js
@@ -0,0 +1,22 @@
+const arrivalDate = document.getElementById("arrival-date");
+
+arrivalDate.addEventListener("blur", ($event) => {
+  const dateSelected = $event.target.value;
+  // Verifier si dateSeleted est en juillet - aout
+  // {
+  /*  dateFormat: "Y-m-d",
+  true
+    disable: [
+        {
+            from: "2025-04-01", from arrival date
+            to: "2025-05-01" until arrival date +7
+        },
+        {
+            from: "2025-09-01", from arival date
+            to: "2025-12-01" until arrival date +3
+        }
+    ]
+} */
+})
+
+// pas oublier d'importer
