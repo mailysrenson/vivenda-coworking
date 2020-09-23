58c4a549861c5b1699c59717642b1a5b0e0fff50
Alexis Duchatelet <alexis.duchatelet@gmail.com>
2020-09-23 15:05:05 +0200

    form translated and functional if info is missing

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,12 @@
 class BookingsController < ApplicationController
   
   def create
+    @booking = Booking.new(booking_params)
+    if @booking.save 
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def new
@@ -11,6 +17,12 @@ class BookingsController < ApplicationController
   end
 
   def update
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:arrival_date, :departure_date, :first_name, :last_name, :email, :phone, :adults, :childrens, :baby_bed, :street, :city, :zipcode, :country)
   end
 
 end
