1db9720dce5938d2e4364030a76ea2becb94e7da
Nicolas Emmery <nicolas.emmery@gmail.com>
2020-10-05 15:02:41 +0200

    btn color switch

--- app/javascript/packs/navbar.js
+++ app/javascript/packs/navbar.js
@@ -4,6 +4,9 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
     let background = document.getElementById("navbar-container");
     let fontText = document.getElementsByClassName("font-text");
+    let menuBarWhite = document.getElementById("menu-btn-white");
+
+console.log(menuBarWhite);
   if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
     background.style.backgroundColor = 'white';
     background.style.borderBottom = 'solid 1px lightgrey';
@@ -11,6 +14,9 @@ function scrollFunction() {
     fontText[1].classList.add('color-to-black');
     fontText[2].classList.add('color-to-black');
     fontText[3].classList.add('btn-call');
+    menuBarWhite.classList.remove('menu-bar-white')
+    menuBarWhite.classList.add('menu-bar-green')
+
   } else {
     background.style.borderBottom = 'solid 0px lightgrey';
     background.style.backgroundColor = 'transparent';
@@ -18,5 +24,9 @@ function scrollFunction() {
     fontText[1].classList.remove('color-to-black');
     fontText[2].classList.remove('color-to-black');
     fontText[3].classList.remove('btn-call');
+    // menuBarWhite.innerHTML = `<img src="menuBarGreen.png" alt="coucou">`
+    menuBarWhite.classList.remove('menu-bar-green')
+    menuBarWhite.classList.add('menu-bar-white')
+
   }
 }
