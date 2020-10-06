da6075e80ae017a1e580a3489661e03680cfb128
Nicolas Emmery <nicolas.emmery@gmail.com>
2020-10-06 14:42:47 +0200

    footer - navbar

--- app/javascript/packs/navbar.js
+++ app/javascript/packs/navbar.js
@@ -6,27 +6,39 @@ function scrollFunction() {
     let fontText = document.getElementsByClassName("font-text");
     let menuBarWhite = document.getElementById("menu-btn-white");
 
+
 console.log(menuBarWhite);
   if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
-    background.style.backgroundColor = 'white';
-    background.style.borderBottom = 'solid 1px lightgrey';
-    fontText[0].classList.add('color-to-black');
-    fontText[1].classList.add('color-to-black');
-    fontText[2].classList.add('color-to-black');
+    background.classList.remove('navbar-trans')
+    background.classList.add('navbar-white')
+
+
+    fontText[0].classList.remove('color-to-white');
+    fontText[1].classList.remove('color-to-white', 'vivenda-size-big');
+    fontText[2].classList.remove('color-to-white', 'nossa-size-big');
+
+    fontText[0].classList.add('color-to-green');
+    fontText[1].classList.add('color-to-green', 'vivenda-size-small');
+    fontText[2].classList.add('color-to-green', 'nossa-size-small');
     fontText[3].classList.add('btn-call');
-    menuBarWhite.classList.remove('menu-bar-white')
-    menuBarWhite.classList.add('menu-bar-green')
+
+    menuBarWhite.classList.remove('menu-bar-white');
+    menuBarWhite.classList.add('menu-bar-green');
+
 
   } else {
-    background.style.borderBottom = 'solid 0px lightgrey';
-    background.style.backgroundColor = 'transparent';
-    fontText[0].classList.remove('color-to-black');
-    fontText[1].classList.remove('color-to-black');
-    fontText[2].classList.remove('color-to-black');
+    background.classList.remove('navbar-white')
+    background.classList.add('navbar-trans')
+    fontText[0].classList.remove('color-to-green');
+    fontText[1].classList.remove('color-to-green', 'vivenda-size-small');
+    fontText[2].classList.remove('color-to-green', 'nossa-size-small');
+    fontText[0].classList.add('color-to-white');
+    fontText[1].classList.add('color-to-white', 'vivenda-size-big');
+    fontText[2].classList.add('color-to-white', 'nossa-size-big');
     fontText[3].classList.remove('btn-call');
     // menuBarWhite.innerHTML = `<img src="menuBarGreen.png" alt="coucou">`
-    menuBarWhite.classList.remove('menu-bar-green')
-    menuBarWhite.classList.add('menu-bar-white')
+    menuBarWhite.classList.remove('menu-bar-green');
+    menuBarWhite.classList.add('menu-bar-white');
 
   }
 }
