29dcb6528e988e513ac9937b28e8c8ac9c687038
Abdel Laqdim <al@kidsandplay.be>
2020-10-08 14:02:41 +0200

    flatpickr issues

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -5,36 +5,45 @@ const initFlatPicker = () => {
     altInput: true,
     altFormat: "d-m-Y",
     dateFormat: "Y-m-d",
-    minDate: "today"
+    minDate: "today",
+    inline: true,
+    mode: "range",
   });
 
-  const arrivalDate = document.getElementsByClassName("form-control string required datepicker-arrival input");
-  arrivalDate[0].addEventListener("blur", ($event) => {
-    if ( arrivalDate[0].value != ""){
-    let dateSelected = $event.target.value;
-      let [day, month, year] = dateSelected.split("-");
-      day = parseInt(day, 10);
-      month = parseInt(month, 10) - 1;
-      year = parseInt(year, 10);
-      let dateDeparture = new Date(year, month, day);
-      if (month === 6 || month === 7 ){
-      dateDeparture.setDate(dateDeparture.getDate() + 7);
-    } else {
-      dateDeparture.setDate(dateDeparture.getDate() + 3);
-    }
-  flatpickr(".datepicker-departure", {
-    altInput: true,
-    altFormat: "d-m-Y",
-    dateFormat: "Y-m-d",
-    minDate: dateDeparture,
-    disable: [ function(date) {
-      if (date < dateDeparture ) {
-        return true
-      }
-    } ]
-  });
-}
+const calendar = document.querySelector(".dayContainer");
+calendar.addEventListener('mousedown', () => {
+  console.log('prout');
+  console.log(calendar);
+  const arrivalDate = document.getElementsByClassName("startRange");
+  console.log(arrivalDate.getDate());
 });
+//   arrivalDate[0].addEventListener("blur", ($event) => {
+//    console.log(arrivalDate)
+//     if ( arrivalDate[0].value != ""){
+//     let dateSelected = $event.target.value;
+//       let [day, month, year] = dateSelected.split("-");
+//       day = parseInt(day, 10);
+//       month = parseInt(month, 10) - 1;
+//       year = parseInt(year, 10);
+//       let dateDeparture = new Date(year, month, day);
+//       if (month === 6 || month === 7 ){
+//       dateDeparture.setDate(dateDeparture.getDate() + 7);
+//     } else {
+//       dateDeparture.setDate(dateDeparture.getDate() + 3);
+//     }
+//   flatpickr(".datepicker-departure", {
+//     altInput: true,
+//     altFormat: "d-m-Y",
+//     dateFormat: "Y-m-d",
+//     minDate: dateDeparture,
+//     disable: [ function(date) {
+//       if (date < dateDeparture ) {
+//         return true
+//       }
+//     } ]
+//   });
+// }
+// });
 };
 
 export {initFlatPicker};
