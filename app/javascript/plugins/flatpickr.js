450882a5236e5bb42818d6cc708eabbbf2a012d9
Nicolas Emmery <nicolas.emmery@gmail.com>
2020-10-09 09:21:02 +0200

    feature min 3 day reservation

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,5 +1,36 @@
 import flatpickr from "flatpickr";
 
+let dateDeparture
+const calendar = document.querySelector(".datepicker-arrival");
+calendar.addEventListener('input', ($event) => {
+  const arrivalDate = document.getElementsByClassName("startRange");
+  let dateSelectedObj = $event.target.value
+
+      let test = document.querySelector(".startRange")
+     let siblingOne = test.nextSibling;
+     siblingOne.classList.add("flatpickr-disabled");
+    let siblingTwo = siblingOne.nextSibling;
+    siblingTwo.classList.add("flatpickr-disabled");
+    let siblingThree = siblingTwo.nextSibling;
+    siblingThree.classList.add("flatpickr-disabled");
+
+
+ let arrivalDateObj = $event.target.value
+      if (arrivalDateObj != ""){
+     let dateSelected = $event.target.value;
+       let [year, month, day] = dateSelected.split("-");
+       day = parseInt(day, 10);
+       month = parseInt(month, 10) - 1;
+       year = parseInt(year, 10);
+       dateDeparture = new Date(year, month, day);
+       if (month === 6 || month === 7 ){
+     dateDeparture.setDate(dateDeparture.getDate() + 7);
+   } else {
+     dateDeparture.setDate(dateDeparture.getDate() + 3);
+   }
+  }
+});
+// <span class="flatpickr-day notAllowed" aria-label="October 15, 2020" tabindex="-1">15</span>
 const initFlatPicker = () => {
     flatpickr(".datepicker-arrival", {
     altInput: true,
@@ -7,16 +38,14 @@ const initFlatPicker = () => {
     dateFormat: "Y-m-d",
     minDate: "today",
     inline: true,
-    mode: "range",
+    mode: "range"
+    // disable: [ function(date) {
+    //   if (date < dateDeparture)
+    //    return true
+    //   }]
   });
 
-const calendar = document.querySelector(".dayContainer");
-calendar.addEventListener('mousedown', () => {
-  console.log('prout');
-  console.log(calendar);
-  const arrivalDate = document.getElementsByClassName("startRange");
-  console.log(arrivalDate.getDate());
-});
+
 //   arrivalDate[0].addEventListener("blur", ($event) => {
 //    console.log(arrivalDate)
 //     if ( arrivalDate[0].value != ""){
