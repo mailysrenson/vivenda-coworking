65781eb3170cd70810bc3d78036ebfafa460aeee
Nicolas Emmery <nicolas.emmery@gmail.com>
2020-10-15 15:27:34 +0200

    add column special_status to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_28_132706) do
+ActiveRecord::Schema.define(version: 2020_10_15_132249) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 2020_09_28_132706) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "language"
+    t.string "special_status"
     t.index ["casas_id"], name: "index_bookings_on_casas_id"
   end
 
