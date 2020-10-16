5c1e99c0200fe567e6a4d2cb145fb49748d02d47
Abdel Laqdim <al@kidsandplay.be>
2020-10-16 11:58:09 +0200

    form css bootstrap

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -8,30 +8,44 @@ const initFlatPicker = () => {
     minDate: "today",
     inline: true,
     mode: "range",
+    disable: [function(date) {
+      let result = rangeDisable(date)
+      console.log(`result `, result)
+        return result;
+    }]
   });
 
-const calendar = document.querySelector(".dayContainer");
-calendar.addEventListener('mousedown', () => {
-  console.log('prout');
-  console.log(calendar);
-  const arrivalDate = document.getElementsByClassName("startRange");
-  console.log(arrivalDate.getDate());
+const calendar = document.querySelector(".flatpickr-input");
+let dateDeparture
+calendar.addEventListener('input', ($event) => {
+  const arrivalDateObject = document.querySelector(".flatpickr-day.selected.startRange");
+  let arrivalDate = arrivalDateObject.dateObj;
+    if ( arrivalDate != ""){
+    let dateSelected = $event.target.value;
+      let [year, month, day] = dateSelected.split("-");
+      day = parseInt(day, 10);
+      month = parseInt(month, 10) - 1;
+      year = parseInt(year, 10);
+      dateDeparture = new Date(year, month, day);
+      if (month === 6 || month === 7 ){
+      dateDeparture.setDate(dateDeparture.getDate() + 7);
+    } else {
+      dateDeparture.setDate(dateDeparture.getDate() + 3);
+    };
+    };
+
 });
+function rangeDisable(date) {
+  console.log(`Departure`, dateDeparture)
+  console.log(`Date`, date)
+  if (date < dateDeparture ) {
+    console.log(`dans if`)
+         return true;
+    }
+}
 //   arrivalDate[0].addEventListener("blur", ($event) => {
 //    console.log(arrivalDate)
-//     if ( arrivalDate[0].value != ""){
-//     let dateSelected = $event.target.value;
-//       let [day, month, year] = dateSelected.split("-");
-//       day = parseInt(day, 10);
-//       month = parseInt(month, 10) - 1;
-//       year = parseInt(year, 10);
-//       let dateDeparture = new Date(year, month, day);
-//       if (month === 6 || month === 7 ){
-//       dateDeparture.setDate(dateDeparture.getDate() + 7);
-//     } else {
-//       dateDeparture.setDate(dateDeparture.getDate() + 3);
-//     }
-//   flatpickr(".datepicker-departure", {
+//   flatpickr(".datepicker-", {
 //     altInput: true,
 //     altFormat: "d-m-Y",
 //     dateFormat: "Y-m-d",
