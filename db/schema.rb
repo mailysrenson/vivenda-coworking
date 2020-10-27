12641fddacc3694cde16512e1bd1adcd2db20971
mailys renson <mailys_94@hotmail.com>
2020-10-27 10:28:09 +0100

    changed zipcode from integer to string

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_16_095029) do
+ActiveRecord::Schema.define(version: 2020_10_27_092603) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,7 +56,7 @@ ActiveRecord::Schema.define(version: 2020_10_16_095029) do
     t.string "state"
     t.string "street"
     t.string "city"
-    t.integer "zipcode"
+    t.string "zipcode"
     t.string "country"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
