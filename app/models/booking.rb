af93fe8b7f9ae4069b8b0b47c13ed25ca4b5ada3
mailys renson <mailys_94@hotmail.com>
2020-11-15 17:21:20 +0100

    added the confirmed and paid / confirmed waiting for payment to admin

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,7 +4,8 @@ class Booking < ApplicationRecord
   validates :adults, :childrens, inclusion: { in: (0..5).to_a}
   scope :pending, ->{where(state: :pending)}
   scope :deposit_requested, ->{where(state: :waiting_for_deposit)}
-  scope :confirmed, ->{where(state: :confirmed)}
+  scope :confirmed_and_paid, ->{where(state: :confirmed_and_paid)}
+  scope :confirmed_waiting_for_payment, ->{where(state: :confirmed_waiting_for_payment)}
   scope :denied, ->{where(state: :denied)}
   scope :closed, ->{where(state: :closed)}
   scope :refund_pending, ->{where(state: :refund_pending)}
@@ -13,15 +14,15 @@ class Booking < ApplicationRecord
   scope :clients, ->{where(special_status: "Aucun")}
   scope :maintenance, ->{where(special_status: "Maintenance")}
 
-  STATE = ['pending', 'waiting_for_deposit', 'confirmed', 'refund_pending', 'cancelled', 'denied', 'closed']
+  STATE = ['pending', 'waiting_for_deposit', 'confirmed_waiting_for_payment', 'confirmed_and_paid' 'refund_pending', 'cancelled', 'denied', 'closed']
   SPECIAL = ['Aucun', 'Famille', 'Maintenance']
 
-  # States: :pending, :waiting_for_deposit, :confirmed, :refund_pending, :cancelled, :denied, :closed
+  # States: :pending, :waiting_for_deposit, :confirmed_waiting_for_payment, :refund_pending, :cancelled, :denied, :closed
 
   state_machine :state, initial: :pending do
   # When creating the booking --> mail
   # after_transition pending: :waiting_for_deposit, do: :send_mail # Mail App : (1) Mail to customer booking received, (2) Mail to owner new booking awaiting
-  # after_transition waiting_for_deposit: :confirmed, do: :send_confirmation_mail
+  # after_transition waiting_for_deposit: :confirmed_waiting_for_payment, do: :send_confirmation_mail
   # after_transition waiting_for_deposit: :cancelled, do :send_cancelling_mail
   # after_transition pending: :denier, do :send_denial_mail
 
@@ -33,16 +34,24 @@ class Booking < ApplicationRecord
       transition pending: :denied
     end
 
-    event :confirm do
-      transition waiting_for_deposit: :confirmed
+    event :confirm_wait_for_payment do
+      transition waiting_for_deposit: :confirmed_waiting_for_payment
+    end
+
+    event :confirm_and_paid do
+      transition confirmed_waiting_for_payment: :confirmed_and_paid
     end
 
     event :close do
-      transition confirmed: :closed
+      transition confirmed_and_paid: :closed
     end
 
     event :refund_asked do
-      transition confirmed: :refund_pending
+      transition confirmed_waiting_for_payment: :refund_pending
+    end
+
+    event :refund_asked do
+      transition confirmed_and_paid: :refund_pending
     end
 
     event :cancel do
